30274f2e232f27dbdcf9d7223685a34829f881e6
Seo <kirin2211@naver.com>
2024-05-21 11:09:54 +0900

    [REF/#62]Joy 커밋 주석 없애기

--- DDooing/DDooing/Views/MainView.swift
+++ DDooing/DDooing/Views/MainView.swift
@@ -44,29 +44,29 @@ struct MainView: View {
                     if let code = document.data()?["code"] as? String {
                         db.collection("Users").whereField("code", isEqualTo: code)
                             .getDocuments { (querySnapshot, error) in
-//                                if let error = error {
-//                                    print("Error getting documents: \(error)")
-//                                } else {
-//                                    let filteredDocuments = querySnapshot?.documents.filter { $0.documentID != currentUserUID }
-//                                    if let partnerDocument = filteredDocuments?.first {
-//                                        self.partnerUID = partnerDocument.documentID
-//                                    }
-//                                    print("파트너 uid : \(String(describing: partnerUID))")
-//                                } 
-                                
                                 if let error = error {
                                     print("Error getting documents: \(error)")
-                                } else if let querySnapshot = querySnapshot, !querySnapshot.documents.isEmpty {
-                                    let document = querySnapshot.documents.first
-                                    if let uidB = document?["uid"] as? String {
-                                        self.partnerUID = uidB
-                                        if let nonOptionalUID = self.partnerUID {
-                                            print("파트너 uid: \(nonOptionalUID)")
-                                        }
-                                    }
                                 } else {
-                                    print("사용자를 찾을 수 없습니다.")
-                                }
+                                    let filteredDocuments = querySnapshot?.documents.filter { $0.documentID != currentUserUID }
+                                    if let partnerDocument = filteredDocuments?.first {
+                                        self.partnerUID = partnerDocument.documentID
+                                    }
+                                    print("파트너 uid : \(String(describing: partnerUID))")
+                                } 
+                                
+//                                if let error = error {
+//                                    print("Error getting documents: \(error)")
+//                                } else if let querySnapshot = querySnapshot, !querySnapshot.documents.isEmpty {
+//                                    let document = querySnapshot.documents.first
+//                                    if let uidB = document?["uid"] as? String {
+//                                        self.partnerUID = uidB
+//                                        if let nonOptionalUID = self.partnerUID {
+//                                            print("파트너 uid: \(nonOptionalUID)")
+//                                        }
+//                                    }
+//                                } else {
+//                                    print("사용자를 찾을 수 없습니다.")
+//                                }
                             }
                     }
                 }
